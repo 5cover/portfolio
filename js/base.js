d3be93ede05f3c2e7c9fa55bb851d2ed26385b9c
Scover <thediscover22450@gmail.com>
2024-06-15 13:06:10 +0200

    project search

--- js/base.js
+++ js/base.js
@@ -1,5 +1,3 @@
-/* Base script */
-
 // Theme switch behavior
 const themeIdPrefix = 'theme-switch-';
 
@@ -12,7 +10,7 @@ const theme = (() => {
 
 document.querySelector('#' + themeIdPrefix + theme).checked = true;
 
-switches = document.querySelectorAll('.theme-switches input[type="radio"]');
+const switches = document.querySelectorAll('.theme-switches input[type="radio"]');
 for (let i = 0; i < switches.length; i++) {
     switches[i].addEventListener('change', _ => {
         var theme = switches[i].id.substring(themeIdPrefix.length);
@@ -21,6 +19,7 @@ for (let i = 0; i < switches.length; i++) {
     });
 }
 
+// todo: do we really need jQuery for this?
 jQuery(document).ready(($) => {
     function showTooltip() {
         $(this).siblings('.definition-tooltip').addClass('definition-tooltip-visible');
@@ -34,3 +33,76 @@ jQuery(document).ready(($) => {
         $('.definition-tooltip-trigger').on('mouseleave focusout', hideTooltip);
     });
 });
+
+/**
+ * @template T
+ * @template TResult
+ * @param {(arg: T) => TResult} transform
+ * @param {TResult} fallback
+ * @param {T} value
+ * @returns { TResult }
+ */
+function map(transform, fallback, value) {
+    return value ? transform(value) : fallback;
+}
+
+/**
+ * @param {boolean} isThemedSvg
+ * @param {string} url
+ * @param {string|undefined} title
+ * @param {Array<string>|undefined} classList
+ * @returns { Promise<Element> }
+ */
+async function getIconElement(isThemedSvg, url, title = undefined, classList = undefined) {
+    let element;
+    if (isThemedSvg) {
+        const content = await fetch(url).then(r => r.text());
+        element = single(elementsFromHTML(content));
+        if (title !== undefined) element.setAttribute('title', title);
+    } else {
+        element = document.createElement('img');
+        element.src = url;
+        if (title !== undefined) element.alt = element.title = title;
+        element.loading = 'lazy';
+    }
+    if (classList !== undefined) element.classList.add(...classList);
+    return element;
+}
+
+/**
+ * @param {string} str 
+ * @returns {string}
+ */
+function ucfirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+/**
+ * @param {String} html HTML representing a single element.
+ * @param {Boolean} trim flag representing whether or not to trim input whitespace, defaults to true.
+ * @return {HTMLCollection}
+ */
+function elementsFromHTML(html, trim = true) {
+    // Process the HTML string.
+    html = trim ? html.trim() : html;
+
+    // Then set up a new template element.
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    const result = template.content.children;
+
+    // Then return either an HTMLElement or HTMLCollection,
+    // based on whether the input HTML had one or more roots.
+    return result;
+}
+/**
+ * @template T
+ * @param {ArrayLike<T>} iter 
+ * @return {T}
+ */
+function single(iter) {
+    if (iter.length === 1) {
+        return iter[0];
+    }
+    throw `expected 1 item, got ${iter.length}`;
+}
---
+++ js/projects.js
@@ -0,0 +1,104 @@
+jQuery(document).ready(async $ => {
+    const titleDataList = document.getElementById('project-titles');
+    const projectList = document.getElementById('project-list');
+    const searchInput = document.getElementById('search-input');
+    const listTags = document.getElementById('list-tag');
+
+    // Fetch project data from JSON file
+
+    const [projects, tags, anchors] = await Promise.all([
+        fetch('/portfolio/data/fr/projects.json').then(res => res.json()).catch(err => { throw err }),
+        fetch('/portfolio/data/fr/tags.json').then(res => res.json()).catch(err => { throw err }),
+        fetch('/portfolio/data/anchors.json').then(res => res.json()).catch(err => { throw err }),
+    ]);
+
+    const allowedTags = new Set();
+    const requestedTag = new URLSearchParams(window.location.search).get('tag');
+    if (requestedTag !== null) {
+        allowedTags.add(requestedTag);
+    }
+
+    // Fill the title datalist
+    for (const id in projects) {
+        const option = document.createElement('option');
+        option.innerHTML = projects[id].title;
+        titleDataList.appendChild(option);
+    }
+
+    // Create tags in the tag list
+    for (const id in tags) {
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.value = id;
+        input.checked = allowedTags.has(id);
+
+        const label = document.createElement('label');
+        label.innerHTML = tags[id];
+
+        const htmlId = 'tag-' + id;
+        label.htmlFor = input.id = htmlId;
+
+        label.appendChild(input);
+        listTags.appendChild(label);
+    }
+
+    document.querySelectorAll('input').forEach(input => {
+        if (input.type === 'checkbox') {
+            input.addEventListener('change', () => {
+                if (input.checked) {
+                    allowedTags.add(input.value);
+                }
+                else {
+                    allowedTags.delete(input.value);
+                }
+                renderProjectList();
+            });
+        } else {
+            input.addEventListener('change', renderProjectList);
+        }
+    });
+
+    // Initial rendering
+    await renderProjectList(requestedTag);
+
+    async function renderProjectList() {
+        const searchTerm = searchInput.value || '';
+        const sortBy = document.querySelector('input[name="sorting"]:checked').value; // asc or desc
+        const filteredProjects = Object.entries(projects)
+            .filter(([_, project]) =>
+                project.title.toLowerCase().includes(searchTerm.toLowerCase())
+                && Array.from(allowedTags).every(t => project.tags.includes(t)))
+            .sort((/** @type {{ title: string; }} */ a, /** @type {{ title: string; }} */ b) => {
+                // Sort by title
+                if (sortBy === 'asc') {
+                    return a[1].title.localeCompare(b[1].title);
+                } else {
+                    return b[1].title.localeCompare(a[1].title);
+                }
+            });
+
+        // Generate the project list HTML
+        const projectListHTML = (await Promise.all(filteredProjects.map(([id, project]) => getProjectHtml(id, project)))).join('');
+
+        // Update the project list
+        projectList.innerHTML = projectListHTML;
+    }
+
+    async function getProjectHtml(id, project) {
+        return `<li${map(b => ` style="--bg-img: url(${b})"`, '', project.background)}>
+        <ul class="list-tag">${project.tags.map(id => `<li><a href="#${id}">${tags[id]}</a></li>`).join('')}</ul>
+            ${await map(async info => (await getIconElement(info.isThemedSvg, `${info.url}`, `Logo ${project.title}`, ['logo'])).outerHTML,
+            '', project.logo)}
+            <h3><a href="project/${id}.html">${project.title}</a></h3>
+            <p class="context"><small>${ucfirst(project.context)}</small></p>
+            <p class="status"><small>${project['start-date']} – ${project['end-date'] || 'en cours'}</small></p>
+            <p class="abstract">${project.abstract}</p>
+            <ul class="list-anchors">
+                ${(await Promise.all(Object.entries(project.anchors).map(async ([name, anchor]) => {
+                const icon = await getIconElement(anchors[anchor.id].isThemedSvg, anchors[anchor.id].url);
+                return `<li><a href="${anchor.href}" title="${name}" target="_blank">${icon.outerHTML}</a></li>`;
+            }))).join('')}
+            </ul>
+        </li>`
+    }
+});
