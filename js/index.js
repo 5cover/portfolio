afec98b5056f27531c97b5f5f8322e5a12a11116
Scover <thediscover22450@gmail.com>
2024-11-26 18:29:29 +0100

    fix spelling mistake

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(_ => {
     const duration = 200;
     const wait = 100;
-    var i = 0;
+    let i = 0;
 
     document.querySelectorAll('.list-piano-tiles > li').forEach(tile => {
         setTimeout(() => tile.classList.add('hover'), i * wait);
