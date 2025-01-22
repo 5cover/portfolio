58803d874fa28012c923bba34c759ece333f5492
Scover <bardini.raphael@gmail.com>
2025-01-22 12:12:20 +0100

    final boom

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
-$(document).ready(_ => {
+$(document).ready(() => {
     const duration = 200;
     const wait = 100;
     let i = 0;
 
-    document.querySelectorAll('.list-piano-tiles > li').forEach(tile => {
+    const tiles = document.querySelectorAll('.list-piano-tiles > li');
+
+    tiles.forEach(tile => {
         setTimeout(() => tile.classList.add('hover'), i * wait);
         setTimeout(() => tile.classList.remove('hover'), i * wait + duration);
         i++;
     });
+
+    setTimeout(() => {
+        for (const t of tiles) t.classList.add('hover');
+    }, (tiles.length - 1) * wait + duration + 1000);
+
+    setTimeout(() => {
+        for (const t of tiles) t.classList.remove('hover');
+    }, (tiles.length - 1) * wait + duration + 1000 + duration);
 });
